4edde076c0eb0670c4707862dc2816cfb7dd5ccc
fasikakassahun <fasikakassahun1912@gmail.com>
2026-08-08 13:12:42 +0300

    Bank Management System With console App Simulation V0.1

--- obj/Debug/net10.0/BankManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/BankManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2da05291cebdf5824d8510b72b16ea26ebc6fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
